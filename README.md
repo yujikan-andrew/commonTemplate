c21f521a5d625714b09dbaf5ab13355ac9c95246
dupeiduo <dupeiduo@agrisz.com>
2020-08-08 15:11:25 +0800

    some supplies

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # common tempalte
 
-## 模版包含 vuex mock API-base
+## 说明
+
+> 路由配置：src/router
+
+> 前端自测模拟数据：mock 
+
+> 数据请求层：src/api
+
+> 全局引入样式，一定要使用这里的范式：src/assets/style/params.less
+
+> 缓存全局数据 vuex： src/store
+
+> 数据请求层，只负责请求数据 src/api
+
+> 数据模型层，只负责处理数据，把结构化数据给 views: src/models
+
+> 页面，减少业务逻辑，尽量只负责渲染: src/views
+
+> 整体布局： src/layout
+
+> 项目通用组件： src/components
 
 ## Project setup
 ```
@@ -24,3 +44,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
---
+++ src/components/tree.vue
@@ -0,0 +1,164 @@
+<template>
+  <ul class="tree-g-item">
+    <li v-for="(item, index_) in treeData">
+      <div class="tree-item no-select pr"
+        :class="{ selected: item.selected }"
+        @click="nodeClick(item)">
+        
+        <span v-if="item.nodes" class="toggle-icon ps" 
+          :class="item.isOpen ? 'el-icon-caret-bottom' : 'el-icon-caret-right'"
+          @click.stop="toggle(item)"/>
+        
+        <span class="item-name">
+          <i class='el-icon-folder'></i>{{ item.name }}
+          <span v-if="item.recmd" class="recommend">推荐</span>
+        </span>
+        <span v-if="item.pathNames" class="path-names">{{ item.pathNames }}</span>
+        
+      </div>
+      <ul v-show="item.isOpen" v-if="item.nodes && item.nodes.length > 0" class="tree-p-item">
+        <tree-item
+          class="item"
+          :lastNode="lastNode"
+          :treeData="item.nodes"
+          @setLastNode="setLastNode"
+          @clearLastNode="clearLastNode"
+        ></tree-item>
+      </ul>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'tree-item',
+  props: {
+    lastNode: Object,
+    item: Object,
+    treeData: Array,
+    defaultProps: {
+      children: 'nodes',
+      label: 'name'
+    }
+  },
+  data: function() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    toggle(item) {
+      if (item.nodes) {
+        item.isOpen = !item.isOpen;
+        this.$forceUpdate()
+      }
+    },
+    nodeClick(item) {
+      this.toggle(item)
+
+      if (this.lastNode) {
+        this.clearLastNode()
+      }
+      item.selected = true
+      this.$emit("setLastNode", item)
+    },
+    clearLastNode() {
+      if (this.lastNode) {
+        this.lastNode.selected = false
+        this.$forceUpdate()
+        this.$emit('clearLastNode', this.lastNode)
+      }
+    },
+    setLastNode(item) {
+      this.$emit("setLastNode", item)
+    }
+  }
+}
+</script>
+<style
+  lang="less" scope>
+
+@iconColor: #66b0f8;
+.tree-g-item {
+  width: 100%;
+  .tree-p-item {
+
+  }
+  .tree-item {
+    cursor: pointer;
+    font-size: 16px;
+    width: 100%;
+    padding: 8px 5px;
+    padding-left: 20px;
+      
+    &:hover {
+      color: #333;
+      background-color: @hoverBg;
+      .el-icon-folder {
+        color: @iconColor;
+      }
+      .toggle-icon {
+        color: #fff;
+      }
+    }
+    .item-name {
+      display: inline-block;
+      width: 100%;
+      white-space: nowrap;
+
+      .el-icon-folder {
+        color: @iconColor;
+        padding: 0 5px;
+        font-size: 20px;
+      }
+    }
+    .path-names {
+      margin-left: 28px;
+      color: @subColor;
+    }
+    .recommend {
+      border: 1px solid #ddd;
+      width: 34px;
+      padding: 0 2px;
+      border-radius: 2px;
+      font-size: 14px;
+      font-weight: bold;
+      color: @green;
+    }
+  }
+  .selected {
+    // background-color: @blue;  
+    background: linear-gradient(
+      to right,  rgb(76, 117, 234)0%,
+      rgb(64, 158, 255) 100%);
+    color: #fff;
+    .toggle-icon {
+      color: #fff;
+    }
+    &:hover {
+      color: #fff;
+      background-color: @blue;
+    }
+  }
+  .toggle-icon {
+    position: absolute;
+    left: 0px;
+    color: #696969;
+  }
+}
+  
+
+.item {
+  cursor: pointer;
+}
+.bold {
+  font-weight: bold;
+}
+ul {
+  line-height: 1.5em;
+  list-style-type: dot;
+}
+.tree-p-item {
+  padding-left: 20px;
+}
+
+</style>
