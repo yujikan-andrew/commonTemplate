29084b57a5efdfde25bbea85b07d3f57cb425d16
dupeiduo <dupeiduo@agrisz.com>
2020-08-20 17:59:49 +0800

    无数据时的显示

---
+++ src/components/no_data/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="no-data" :style="{height: height+ 'px', lineHeight: height+ 'px'}">
+    {{ title }}
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  props: {
+    height: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+      default: '暂无数据'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style
+  lang="less" scoped>
+  @import '../../assets/common/index.less';
+  .no-data {
+    font-size: 14px;
+    text-align: center;
+    color: @subFontColor;
+  }
+</style>
