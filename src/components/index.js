94135eb0a68f2c44bf1e74d6b2446f5b41e1fd8b
dupeiduo <du_peiduo@163.com>
2020-12-22 13:09:50 +0800

    命名规范，重命名

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
-import SvgIcon from './sz_svg_icon'// svg component
+import szSvgIcon from './sz_svg_icon'// svg component
 import szLoading from './sz_loading'
 import szTree from './sz_tree'
 import szNoData from './sz_no_data'
-import SzEchart from './sz_echart'
-import SzMap from './sz_map'
+import szEchart from './sz_echart'
+import szMap from './sz_map'
 
 // register globally
-Vue.component('sz-svgicon', SvgIcon)
+Vue.component('sz-svgicon', szSvgIcon)
 Vue.component('sz-loading', szLoading)
 Vue.component('sz-tree', szTree)
 Vue.component('sz-nodata', szNoData)
-Vue.component('sz-echart', SzEchart)
-Vue.component('sz-map', SzMap)
+Vue.component('sz-echart', szEchart)
+Vue.component('sz-map', szMap)
