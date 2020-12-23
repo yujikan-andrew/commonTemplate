960e2294075dee6f73fe457fe52e9f8e023ab18d
dupeiduo <du_peiduo@163.com>
2020-12-23 18:30:23 +0800

    使用 ol 比例尺

--- src/components/sz_supermap/index.vue
+++ src/components/sz_supermap/index.vue
@@ -33,7 +33,11 @@ export default {
     var map, url = "https://iserver.supermap.io/iserver/services/map-china400/rest/maps/China";
     map = new ol.Map({
         target: 'map',
-        controls: ol.control.defaults({attributionOptions: {collapsed: true}}),
+        controls: ol.control.defaults().extend([
+          new ol.control.ScaleLine({
+            target: document.getElementById('scaleline')
+          })
+        ]),
         view: new ol.View({
             center: [11630660.398081223, 4280001.184277043],
             zoom: 4,
@@ -49,7 +53,6 @@ export default {
         projection: 'EPSG:3857'
     });
     map.addLayer(layer);
-    map.addControl(new ol.supermap.control.ScaleLine());
   },
   methods: {
 
